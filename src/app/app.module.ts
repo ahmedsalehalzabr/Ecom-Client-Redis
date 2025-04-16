c49f268c9d000a35001d1b5cc18bae4cd235307e
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-04-16 03:14:41 +0300

    done spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { ShopModule } from './shop/shop.module';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { RouterLink } from '@angular/router';
+import { HomeModule } from './home/home.module';
+import { loaderInterceptor } from './core/Interceptor/loader.interceptor';
 
 @NgModule({
   declarations: [
@@ -19,12 +22,15 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    HomeModule,
+    RouterLink,
     ShopModule,
     NgxSpinnerModule
   ],
   providers: [
     provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withInterceptorsFromDi()),
+    {provide:HTTP_INTERCEPTORS,useClass:loaderInterceptor,multi:true}
   ],
   bootstrap: [AppComponent]
 })

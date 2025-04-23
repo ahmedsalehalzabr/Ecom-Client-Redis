b8a701947b7ccbc416d294ae7f5f1b146793eead
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2025-04-23 07:04:22 +0300

    Create Register Form Validation

--- src/app/identity/register/register.component.ts
+++ src/app/identity/register/register.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { group } from 'console';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+fromGroup:FormGroup;
+constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+   this.formValidation();
+  }
 
+formValidation(){
+this.fromGroup=this.fb.group({
+  UserName:['',[Validators.required,Validators.minLength(6)]],
+  email:['',[Validators.required,Validators.email]],
+  DisplayName:['',[Validators.required]],
+  password:['',[Validators.required,Validators.pattern(/^(?=.*[0-9])(?=.*[#$@!.\-])[A-Za-z\d#$@!.\-]{8,}$/)]]
+})
+}
+get _username() {
+  return this.fromGroup.get('UserName');
+}
+get _email() {
+  return this.fromGroup.get('email');
+}
+get _DisplayName() {
+  return this.fromGroup.get('DisplayName');
+}
+get _password() {
+  return this.fromGroup.get('password');
+}
 }
